7793c9c5d9216ecfbf0ee58b6bb48bf68178f08c
Julian Diaz <torresanibal336@gmail.com>
2025-11-06 18:30:29 -0500

    Se crea un nuevo script y un archivo js para el ejercicio 8

---
+++ Ejercicio8.js
@@ -0,0 +1,13 @@
+// Ejercicio 8
+
+// Bucle para tomar datos de 5 usuarios
+for (let i = 1; i <= 5; i++) {
+    // Solicitamos los datos al usuario
+    let nombre = prompt("Ingrese su nombre");
+    let edad = prompt("Ingrese su edad");
+
+    // Validamos si es mayor de edad
+    if (edad >= 10) {
+        alert(`Hola ${nombre}`)
+    } 
+}
